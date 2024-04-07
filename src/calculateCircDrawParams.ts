335c34087e2df35cc78a9510b3d4dd4c260968c5
BoDee Angus <bodee.angus@gmail.com>
2024-04-06 22:11:51 -0600

    Modified calculateCircDrawParams.ts, last output was erroneous.

--- src/calculateCircDrawParams.ts
+++ src/calculateCircDrawParams.ts
@@ -14,15 +14,44 @@ const calculateCircDrawParams = (letter: string, index: number, color: string, c
 
   // After these two points are defined, the point that lies directly in between both of them will be the center of the new circle.
   // The radius of the circle is defined as the distance of either of the two points to the center of the circle, so the circle should intersect them.
+  const centerX = canvas.width / 2;
+  const centerY = canvas.height / 2;
+  const mainRadius = canvas.height * 0.8 / 2;
+
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const letterIndex = alphabet.indexOf(letter.toUpperCase());
+  const totalLetters = alphabet.length;
+
+  const firstPointDeg = (letterIndex / totalLetters) * 360;
+  const secondPointDeg = 180 + (index * 360) / totalLetters;
+
+  const firstPointRad = (firstPointDeg * Math.PI) / 180;
+  const secondPointRad = (secondPointDeg * Math.PI) / 180;
+
+  const firstPointX = centerX + mainRadius * Math.cos(firstPointRad);
+  const firstPointY = centerY + mainRadius * Math.sin(firstPointRad);
+
+  const secondPointX = centerX + mainRadius * Math.cos(secondPointRad);
+  const secondPointY = centerY + mainRadius * Math.sin(secondPointRad);
+
+  const newCenterX = (firstPointX + secondPointX) / 2;
+  const newCenterY = (firstPointY + secondPointY) / 2;
+
+  const radius = Math.sqrt(
+    Math.pow(firstPointX - newCenterX, 2) + Math.pow(firstPointY - newCenterY, 2)
+  );
+
+  const startDeg = 0;
+  const endDeg = 360;
 
   return {
     ctx,
-    centerX,
-    centerY,
+    centerX: newCenterX,
+    centerY: newCenterY,
     radius,
     startDeg,
     endDeg,
-    color
+    color,
   };
 };
 
